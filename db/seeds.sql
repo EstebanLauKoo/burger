be41b030842cd749a6ae5d88a742729b9cbfd15d
Esteban lau koo <Esteban lau koo>
2017-05-02 01:10:36 -0400

    basic logic is working

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('McChicken', TRUE, CURRENT_TIMESTAMP );
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('McDouble', FALSE, CURRENT_TIMESTAMP );
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Whopper', FALSE, CURRENT_TIMESTAMP );
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Fish Filet', TRUE, CURRENT_TIMESTAMP );
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Protein Style', FALSE, CURRENT_TIMESTAMP );
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Baconator', TRUE, CURRENT_TIMESTAMP );
